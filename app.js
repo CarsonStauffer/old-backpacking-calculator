0d9f0c6992d394a9d3dd650306661770fcc75aa3
Carson Stauffer <carsonstauffer@gmail.com>
2019-02-14 12:40:40 -0700

    Sort prices table

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ function generatePricesTable() {
   tbody.append(generatePricesRow("laos"));
   tbody.append(generatePricesRow("malaysia"));
   tbody.append(generatePricesRow("indonesia"));
+  sortTable();
 }
 
 function setPreference(preference) {
@@ -51,4 +52,27 @@ function setPreference(preference) {
       break;
   }
   generatePricesTable();
+}
+
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("prices");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 0; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = parseInt(rows[i].getElementsByTagName("TD")[1].innerHTML.replace('/day', '').replace('$', ''));
+      y = parseInt(rows[i + 1].getElementsByTagName("TD")[1].innerHTML.replace('/day', '').replace('$', ''));
+      if (x > y) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
 }
